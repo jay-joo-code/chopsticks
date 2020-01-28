61566f145f216cb662bfc2d05f8c9ad2ea5c76bc
EC2 Default User <ec2-user@ip-172-31-33-164.ap-northeast-1.compute.internal>
2020-01-28 05:49:51 +0000

    fix: cart empty bug

--- client/src/components/pages/Cart/index.jsx
+++ client/src/components/pages/Cart/index.jsx
@@ -7,7 +7,10 @@ import CartEmpty from './CartEmpty';
 const Cart = () => {
   const history = useHistory();
   const user = useSelector((state) => state.user);
-  if (!user) history.push('/login');
+  if (!user) {
+    history.push('/login');
+    return <div />;
+  }
   else if (!user.cart.length) return <CartEmpty />;
   return <CartUI />;
 };
