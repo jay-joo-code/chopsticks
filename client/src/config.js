24eb2a4e9915bbeca44e5cac2dbc44aa95ef364d
EC2 Default User <jj534@cornell.edu>
2020-03-24 09:03:55 +0000

    tracking api credentials env -> cfg

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,6 @@
 export default {
   BASE: '/api',
-  BASE_PROD: ''
+  BASE_PROD: '',
+  T_BASE: 'https://info.sweettracker.co.kr/api/v1',
+  T_KEY: 'kn6HC3dB3XeTq2oosH7i5A'
 }
--- client/src/util/path/trackerUrl.js
+++ client/src/util/path/trackerUrl.js
@@ -1,7 +1,8 @@
+import cfg from 'src/config';
+
 const trackerUrl = (order) => {
   const { companyCode, invoice } = order.deliv;
-  const { REACT_APP_TRACKER_BASE, REACT_APP_TRACKER_KEY } = process.env;
-  return `${REACT_APP_TRACKER_BASE}/trackingInfo?t_key=${REACT_APP_TRACKER_KEY}&t_code=${companyCode}&t_invoice=${invoice}` 
+  return `${cfg.T_BASE}/trackingInfo?t_key=${cfg.T_KEY}&t_code=${companyCode}&t_invoice=${invoice}` 
 }
 
 export default trackerUrl;
