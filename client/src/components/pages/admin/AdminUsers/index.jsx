4a9f999d3c5993507e9bfa09f41f8ff64498d13a
EC2 Default User <jj534@cornell.edu>
2020-04-07 04:54:58 +0000

    applied balsamic admin user list ui

--- client/src/components/pages/admin/AdminUsers/index.jsx
+++ client/src/components/pages/admin/AdminUsers/index.jsx
@@ -3,51 +3,45 @@ import styled from 'styled-components';
 import Heading from 'src/components/common/fonts/Heading';
 import api from 'src/util/api';
 import log from 'src/util/log';
-import Btn from 'src/components/common/buttons/Btn';
 
 const Container = styled.div`
 
 `;
 
-const ToggleFilter = styled(Btn)`
-  margin: 2rem 0;
+const Row = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 2rem;
 `
 
 const AdminUsers = () => {
   const [users, setUsers] = useState([]);
-  const [filterShops, setFilterShops] = useState(false);
   
   useEffect(() => {
     api.get('/user')
       .then((res) => {
-        if (filterShops) {
-          const usersWithShop = res.data.filter((user) => user.shop.accepted);
-          setUsers(usersWithShop)
-        } else {
-          setUsers(res.data);
-        }
+        setUsers(res.data);
       })
       .catch((e) => log(`ERROR AdminUsers`, e))
-  }, [filterShops])
+  }, [])
   
   return (
     <Container>
-      <Heading>유저</Heading>
-      <ToggleFilter
-        onClick={() => setFilterShops(!filterShops)}
-      >
-        모든 유저 / 샵 승인된 유저
-      </ToggleFilter>
+      <Row>
+        <Heading>유저</Heading>
+        <p>{users.length}명</p>
+      </Row>
       <div className="tableContainer">
         <table className="table table-striped table-hover">
           <thead>
             <tr>
               <th>No</th>
-              <th>Name</th>
-              <th>Email</th>
-              <th>Shop</th>
-              <th>Cart Items</th>
-              <th>Created At</th>
+              <th>이메일</th>
+              <th>이름</th>
+              <th>휴대전화</th>
+              <th>샵 정보</th>
+              <th>가입일자</th>
             </tr>
           </thead>
           <tbody>
@@ -57,16 +51,16 @@ const AdminUsers = () => {
                   {i + 1}
                 </td>
                 <td>
-                  {user.name}
-                </td>
-                <td>
                   {user.email}
                 </td>
                 <td>
-                  {user.shop.title}
+                  {user.name}
                 </td>
                 <td>
-                  {user.cart.length}
+                  {user.mobile}
+                </td>
+                <td>
+                  {user.shop.title}
                 </td>
                 <td>
                   {user.shop.createdAt}
