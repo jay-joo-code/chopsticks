43a6d8014032fa3bf7458e118d4b6003f12a494f
EC2 Default User <jj534@cornell.edu>
2020-04-07 05:09:17 +0000

    api config

--- client/src/util/api.js
+++ client/src/util/api.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 import cfg from 'src/config';
 
+const baseURL = process.env.NODE_ENV === 'production' ? process.env.REACT_APP_API_URL : cfg.BASE;
+
 const api = axios.create({
-  baseURL: cfg.BASE
+  baseURL
 });
 
 export default api;
