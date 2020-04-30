01f859a5c75ea05cbde84751105b1ec281188903
Jae Joo <jae@Jaes-MacBook-Air.local>
2020-04-30 17:31:48 +0900

    선택옵션 삭제

--- client/src/components/pages/item/ItemEdit/SellerOpts/Opts/index.jsx
+++ client/src/components/pages/item/ItemEdit/SellerOpts/Opts/index.jsx
@@ -16,17 +16,19 @@ const Container = styled.div`
 const Opts = ({ formik }) => {
   return (
     <Container>
-      <Label>필수 옵션 <Muted>Optional</Muted></Label>
-      <Body muted mb={1}>*필수로 설정해야 하는 기본 옵션을 설정합니다.</Body>
+      <Label>옵션 <Muted>Optional</Muted></Label>
       <AddOptGrpBtn formik={formik} optional={false} />
       <OptGrpList formik={formik} optional={false} />
-      <Label>선택 옵션 <Muted>Optional</Muted></Label>
-      <Body muted mb={1}>*추가로 선택 옵션을 추가할 수 있습니다.</Body>
-      <AddOptGrpBtn formik={formik} optional={true} />
-      <OptGrpList formik={formik} optional={true} />
       <OptDataInput formik={formik} />
     </Container>
   )
 };
+
+/*
+<Label>선택 옵션 <Muted>Optional</Muted></Label>
+<Body muted mb={1}>*추가로 선택 옵션을 추가할 수 있습니다.</Body>
+<AddOptGrpBtn formik={formik} optional={true} />
+<OptGrpList formik={formik} optional={true} />
+*/
 
 export default Opts;
